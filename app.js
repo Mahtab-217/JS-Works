3033dc456ed033ee0f6ad251f4dbb5c4d8f842bd
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-01-13 22:03:07 +0430

    working with numbers and conditions

--- app.js
+++ app.js
@@ -18,3 +18,19 @@ const funny= "Ha";
 const repeated =funny.repeat( 4);
 const name="mahtab";
 name.substring(0,1).toUpperCase()+name.substring(1);
+const lower=sen.toUpperCase();
+
+// homework
+// first task
+const number=-5
+if(number>0 ){
+ console.log("this is a positive number")
+}
+else if(number<0){
+console.log("this number is nagtive")
+}
+else{
+    console.log("this number equals to 0")
+}
+// second task
+
