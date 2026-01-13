f81df1996c9156b8293640adc7668ea26efd1b42
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-01-13 22:11:23 +0430

    voting related to age working with 'if'

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-const sen ="   Hi there dear friends  ";
-const splited=sen.split(' ');
-if(sen.startsWith('Hi')){
-    console.log("It is correct");
-}
-else{
-    console.log("it is uncorrect");
-}
-if(sen.endsWith('Bey')){
-    console.log("You are polite")
-}
-else{
-    console.log("You are rude");
-}
-const trimed=sen.trim();
-const fixed =sen.replaceAll(" ",".");
-const funny= "Ha";
-const repeated =funny.repeat( 4);
-const name="mahtab";
-name.substring(0,1).toUpperCase()+name.substring(1);
-const lower=sen.toUpperCase();
-
-// homework
 // first task
 const number=-5
 if(number>0 ){
@@ -33,4 +10,18 @@ else{
     console.log("this number equals to 0")
 }
 // second task
-
+const num=42;
+if(num%2===0){
+    console.log("this an even number");
+}
+else{
+    console.log("the number is odd");
+}
+// third task
+const age=17;
+if(age>18){
+    console.log("you are able to vote");
+}
+else{
+    console.log("you're unable to vote");
+}
