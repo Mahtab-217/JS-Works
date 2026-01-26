c018406a88439e5d87be87a3d67efb5401df1b59
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-01-26 20:52:29 +0430

    rested parameters

--- app.js
+++ app.js
@@ -15,3 +15,17 @@ const jum =(a,b=32)=>{
    console.log(a+b);
 }
 jum(15)
+
+const rested=(...b)=>{
+    console.log(b)
+}
+rested(3,2,4,5,2,1,44,56)
+
+function lot(...a){
+    let total=0;
+    for(let i=0; i<a.length; i++){
+        total= total+a[i];
+    }
+    console.log(total);
+}
+lot(4,2,6)
