afbe0bf2989418574a64be05d98d32e11f6a24cf
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-01-27 16:10:22 +0430

    new code

--- app.js
+++ app.js
@@ -1,40 +1,55 @@
-function hi(name){
-    console.log("Hi dear" + name)
-}
-hi(" Ali");
-const bye= function(name){
-    console.log("Good bye dear " +name)
-}
-bye("Ahmad");
+// function hi(name){
+//     console.log("Hi dear" + name)
+// }
+// hi(" Ali");
+// const bye= function(name){
+//     console.log("Good bye dear " +name)
+// }
+// bye("Ahmad");
 
-const sum=(a,b)=>{
-    console.log(a*b);
-}
-sum(5,12);
-const jum =(a,b=32)=>{
-   console.log(a+b);
-}
-jum(15)
+// const sum=(a,b)=>{
+//     console.log(a*b);
+// }
+// sum(5,12);
+// const jum =(a,b=32)=>{
+//    console.log(a+b);
+// }
+// jum(15)
 
-const rested=(...b)=>{
-    console.log(b)
-}
-rested(3,2,4,5,2,1,44,56)
+// const rested=(...b)=>{
+//     console.log(b)
+// }
+// rested(3,2,4,5,2,1,44,56)
 
-function lot(...a){
-    let total=0;
-    for(let i=0; i<a.length; i++){
-        total= total+a[i];
-    }
-    console.log(total);
-}
-lot(4,2,6);
+// function lot(...a){
+//     let total=0;
+//     for(let i=0; i<a.length; i++){
+//         total= total+a[i];
+//     }
+//     console.log(total);
+// }
+// lot(4,2,6);
 
-function multiply(...b){
-     let result=1;
-     for(let i=1; i<b.length; i++){
-        result= result*b[i];
-     }
-     console.log(result)
+// function multiply(...b){
+//      let result=1;
+//      for(let i=1; i<b.length; i++){
+//         result= result-b[i];
+//      }
+//      console.log(result)
+// }
+// multiply(3,5,9,90)
+const num=( num1, num2)=>{
+   if(num1==num2){
+    console.log("Equal")
+   }
+   else if(num1 >num2){
+    console.log("number one is greater")
+
+   }
+   else{
+    console.log("number two is greater")
+   }
 }
-multiply(3,5)
+num(34,43)
+num(21, 12)
+num(22,22)
