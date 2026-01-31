111c4e4b5981f0af1157fd657e90ef2af5794455
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-01-31 22:07:48 +0430

    new codes

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ console.log(text.charAt(4));
 console.log(text.charCodeAt(0));
 console.log(text.indexOf("f"));
 console.log(text.lastIndexOf("f"));
+console.log(text.includes("name"));
+console.log(text.startsWith("Hello"));
+console.log(text.endsWith(" "));
+
+const sen="Hello World I'm busy";
+console.log(replace("H", "I"));
+console.log(sen.repeat())
