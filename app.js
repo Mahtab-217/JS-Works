ab842a244e3761563e9941a4ce74154d1f7694fa
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-02-02 21:54:11 +0430

    new small works

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log(text.toLocaleLowerCase());
 console.log(text.toUpperCase());
 console.log(text.split(" "));
 
-const jumla="My friends are great"
+const jumla="   My friends are great    "
 console.log(jumla.trim());
 console.log(jumla.charAt(4));
 
@@ -12,6 +12,7 @@ const para="God is the kindest"
 console.log(para.charCodeAt(0));
 console.log(para.indexOf("t"));
 console.log(para.lastIndexOf("t"));
+console.log(para.replace("God","Allah"));
 
 const aziz="Dear classmates be in touch";
 console.log(aziz.includes("be"));
@@ -22,10 +23,10 @@ const sen="Hello ";
  console.log(sen.repeat(5));
  console.log(sen.slice(0,3));
  console.log(sen.concat("JS"));
+ console.log(sen.substring(1,3));
 
  const sentence="Ali Ahmadi";
- console.log(sentence.localeCompare("d"));
-
+ console.log(sentence.localeCompare("Ahmadi"));
 
  const number=6;
- console.log(number.padStart(3,"0"))
+ console.log(number.padStart(3,"0"  ))
