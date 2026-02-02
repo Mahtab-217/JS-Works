a72b14b474e0f9bc433fc72caefc1a8f3484249c
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-02-02 21:58:25 +0430

    new codes

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-const text="  In the name of God ";
-console.log(text.length);
-console.log(text.toLocaleLowerCase());
-console.log(text.toUpperCase());
-console.log(text.split(" "));
+// const text="  In the name of God ";
+// console.log(text.length);
+// console.log(text.toLocaleLowerCase());
+// console.log(text.toUpperCase());
+// console.log(text.split(" "));
 
-const jumla="   My friends are great    "
-console.log(jumla.trim());
-console.log(jumla.charAt(4));
+// const jumla="   My friends are great    "
+// console.log(jumla.trim());
+// console.log(jumla.charAt(4));
 
-const para="God is the kindest"
-console.log(para.charCodeAt(0));
-console.log(para.indexOf("t"));
-console.log(para.lastIndexOf("t"));
-console.log(para.replace("God","Allah"));
+// const para="God is the kindest"
+// console.log(para.charCodeAt(0));
+// console.log(para.indexOf("t"));
+// console.log(para.lastIndexOf("t"));
+// console.log(para.replace("God","Allah"));
 
-const aziz="Dear classmates be in touch";
-console.log(aziz.includes("be"));
-console.log(aziz.startsWith("Dear"));
-console.log(aziz.endsWith(" "));
+// const aziz="Dear classmates be in touch";
+// console.log(aziz.includes("be"));
+// console.log(aziz.startsWith("Dear"));
+// console.log(aziz.endsWith(" "));
 
-const sen="Hello ";
- console.log(sen.repeat(5));
- console.log(sen.slice(0,3));
- console.log(sen.concat("JS"));
- console.log(sen.substring(1,3));
+// const sen="Hello ";
+//  console.log(sen.repeat(5));
+//  console.log(sen.slice(0,3));
+//  console.log(sen.concat("JS"));
+//  console.log(sen.substring(1,3));
 
- const sentence="Ali Ahmadi";
- console.log(sentence.localeCompare("Ahmadi"));
+//  const sentence="Ali Ahmadi";
+//  console.log(sentence.localeCompare("Ahmadi"));
 
- const number=6;
- console.log(number.padStart(3,"0"  ))
+//  const number=6;
+//  console.log(number.padStart(3,"0"  ))
+
+
