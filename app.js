027920a55a37ca66508a93fcf2ab96b48feed7ea
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-02-03 20:57:59 +0430

    new function

--- app.js
+++ app.js
@@ -30,45 +30,58 @@
 
 //  const number=6;
 //  console.log(number.padStart(3,"0"  ))
-function find(...number){
-    let count=0;
-    let total=0;
-    for(let i=0; i<number.length;i++)
-{
-    if(number[i]>0){
-        count++;
-        total+=number[i];
+// function find(...number){
+//     let count=0;
+//     let total=0;
+//     for(let i=0; i<number.length;i++)
+// {
+//     if(number[i]>0){
+//         count++;
+//         total+=number[i];
+//     }
+// }
+// return total/count;
+// }
+// console.log(find(1,2,4,5,-4));
+
+// function getText(test){
+// let result="";
+// for(let i=0; i<Text.length; i++){
+//     if(text[i]>="a" && text[i]<="z" ||text[i]>="A" && text[i]<="Z"){
+//         result+=text[i];
+//     }
+// }
+// return result;
+// }
+// const outcome= getText("Hell33o");
+
+
+// function snake(a){
+//     let result="";
+//     for(let i=0; i<a.length;i++){
+//         if(a[i]>="A" && a[i]<="Z"){
+//             result += "_"+ a[i].toLowerCase();
+
+//         }
+//         else
+//         {
+//             result +=a[i];
+//         }
+//     }
+//     return result;
+// }
+// console.log(snake("lastName"));
+// console.log(snake("fatherName"));
+// // father_name
+
+function deleteSpace(text){
+ let start="";
+ let end =text.length-1;
+  for(let i=0; i<text.length; i ++){
+    if(text[i]!==" "){
+        start =i;
+        break;
     }
+  }
 }
-return total/count;
-}
-console.log(find(1,2,4,5,-4));
-
-function getText(test){
-let result="";
-for(let i=0; i<Text.length; i++){
-    if(text[i]>="a" && text[i]<="z" ||text[i]>="A" && text[i]<="Z"){
-        result+=text[i];
-    }
-}
-return result;
-}
-const outcome= getText("Hell33o");
-
-
-function snake(a){
-    let result="";
-    for(let i=0; i<a.length;i++){
-        if(a[i]>="A" && a[i]<="Z"){
-            result += "_"+ a[i].toLowerCase();
-
-        }
-        else
-        {
-            result +=a[i];
-        }
-    }
-    return result;
-}
-console.log(snake("lastName"));
-console.log(snake("fatherName"));
+deleteSpace("    Hello Dear Teacher   ")
