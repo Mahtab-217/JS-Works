d1f181d65885298b7c3e1fec0c6e91069d3fec47
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-02-06 21:39:06 +0430

    new functios

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 
-function deleteSpace(){
-    let start="";
-    let end =text.length-1;
-    for(let i= 0; i<text.length; i++){
-        if(text[i]!==" "){
-            start =i;
-            break;
-        }
-    }
-    for(let i=text.length-1; i>=0;i--){
-        if(text[i]!==" "){
-            end=i;
-            break;
-        }
-    }
-    return text.slice(start,end+1);
-}
-const result5 = deleteSpace("    Hello Dear Teacher    ");
+// function deleteSpace(){
+//     let start="";
+//     let end =text.length-1;
+//     for(let i= 0; i<text.length; i++){
+//         if(text[i]!==" "){
+//             start =i;
+//             break;
+//         }
+//     }
+//     for(let i=text.length-1; i>=0;i--){
+//         if(text[i]!==" "){
+//             end=i;
+//             break;
+//         }
+//     }
+//     return text.slice(start,end+1);
+// }
+// const result5 = deleteSpace("    Hello Dear Teacher    ");
 
 
-function reverse(text){
-     let result=text.split(' ').map(word=>{
-        let reverse='';
-        for(let i=0; i>=0; i--){
-          reverse+=word[i];
-        }
-        return reverse;
-     }).join(" ");
+
+// function reverse(text){
+//      let result=text.split(' ').map(word=>{
+//         let reverse='';
+//         for(let i=0; i>=0; i--){
+//           reverse+=word[i];
+//         }
+//         return reverse;
+//      }).join(" ");
    
-     return result;
+//      return result;
+// }
+// const total2 = reverse("Hi Teacher");
+
+function greeting(n){
+   console.log(`We are in numer ${n}`);
 }
-const total2 = reverse("Hi Teacher");
+const list =[1,2,3]
+list.forEach(greeting);
